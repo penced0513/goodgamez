1498937254aac7edde2daabfd8d5fe4a288fc901
Curtis Bridge <c.bridge0496@gmail.com>
2021-07-27 13:18:44 -0400

    seeders for genres and games

--- db/models/game.js
+++ db/models/game.js
@@ -4,16 +4,17 @@ module.exports = (sequelize, DataTypes) => {
     name: DataTypes.STRING,
     description: DataTypes.TEXT,
     img: DataTypes.TEXT,
+    developer: DataTypes.STRING,
     genreId: DataTypes.INTEGER
   }, {});
-  Game.associate = function(models) {
-    Game.belongsTo(models.Genre, { foreignKey: "genreId" } )
-    Game.hasMany(models.Review, { foreignKey: "gameId" } )
+  Game.associate = function (models) {
+    Game.belongsTo(models.Genre, { foreignKey: "genreId" })
+    Game.hasMany(models.Review, { foreignKey: "gameId" })
     const columnMapping = {
       through: "Joinsgamesandshelf",
       foreignKey: "gameId",
       otherKey: "shelfId",
-    }
+    };
     Game.belongsToMany(models.Gameshelf, columnMapping)
   };
   return Game;
---
+++ db/seeders/20210727152851-genre_seeds.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+
+    */
+    return queryInterface.bulkInsert('Genres', [
+      { name: 'Sandbox', createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Real-time Strategy', createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Shooters', createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Multiplayer online battle arena', createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Role-Playing', createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Sports', createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Puzzle', createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Party', createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Action-adventure', createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Survival and Horror', createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Platformer', createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Simulation', createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Fighting', createdAt: new Date(), updatedAt: new Date() }
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+    return queryInterface.bulkDelete('Genres', null, { truncate: true, cascade: true, restartIdentity: true });
+  }
+};
