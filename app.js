2e28919a08677113eda5e9f214daf12657359053
Kristian <kristian.panku@gmail.com>
2021-07-27 16:05:07 -0600

    did a lot of shit man, on god

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const shelvesRouter = require('./routes/shelves');
 const { sessionConfig } = require('./config');
 const { secret } = sessionConfig
 const { restoreUser } = require('./auth')
@@ -41,6 +42,7 @@ store.sync();
 app.use(restoreUser)
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/shelves', shelvesRouter);
 
 
 // catch 404 and forward to error handler
