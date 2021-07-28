f37eaee70d5dc1db67eecba532612e946cdbf0d4
Kristian <kristian.panku@gmail.com>
2021-07-28 17:30:18 -0600

    NOW the changes to db are done

--- config/database.js
+++ config/database.js
@@ -10,11 +10,16 @@ module.exports = {
     host,
     dialect: 'postgres',
     seederStorage: 'sequelize',
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
     dialectOptions: {
       ssl: {
         require: true,
         rejectUnauthorized: false,
       },
     },
-  }
+  },
 }
