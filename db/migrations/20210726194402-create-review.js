2e58f275cac60813c00c3f4ab2d29ac8a691bf75
Shawn Boyle <shawn.boyle01@gmail.com>
2021-07-30 17:59:28 -0600

    styled a majority of games and game pages

--- db/migrations/20210726194402-create-review.js
+++ db/migrations/20210726194402-create-review.js
@@ -10,7 +10,7 @@ module.exports = {
       },
       review: {
         allowNull: false,
-        type: Sequelize.TEXT
+        type: Sequelize.STRING(1000)
       },
       reviewScore: {
         allowNull: false,
